6954566380a7d5533355c1fd003023aa10eea10d
blizzlikegroup <correio13@gmail.com>
2013-10-12 18:04:11 -0300

    Patch: update warden.

--- sql/development/world/world_updates.sql
+++ sql/development/world/world_updates.sql
@@ -6,4 +6,4 @@ UPDATE `creature_template` SET `inhabitType` = '4' WHERE `entry` = '16520';
 UPDATE `creature_template` SET `inhabitType` = '4' WHERE `entry` = '19551';
 
 -- go_silithyst_geyser
-UPDATE `gameobject_template` SET ScriptName = 'go_silithyst_geyser' WHERE `entry` = '181598';
+UPDATE `gameobject_template` SET `ScriptName` = 'go_silithyst_geyser' WHERE `entry` = '181598';
