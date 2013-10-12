ad5559b92c4e7f6233ae805e0d328cdbdbfec05e
blizzlikegroup <correio13@gmail.com>
2013-10-11 22:44:27 -0300

    DB: add update/ go_silithyst_geyser.

--- sql/development/world/world_updates.sql
+++ sql/development/world/world_updates.sql
@@ -4,3 +4,6 @@ UPDATE `creature_template` SET `inhabitType` = '4' WHERE `entry` = '16520';
 -- Ember of Al'ar
 -- old `inhabitType` = '3'
 UPDATE `creature_template` SET `inhabitType` = '4' WHERE `entry` = '19551';
+
+-- go_silithyst_geyser
+UPDATE `gameobject_template` SET ScriptName = 'go_silithyst_geyser' WHERE `entry` = '181598';
