966982bf889f0816e1c7eef0de54a1574c8fe590
ysfl <ysfllxcn@live.com>
2013-10-14 00:25:52 +0800

    Test: Transmogrification

--- src/scripts/system/ScriptLoader.cpp
+++ src/scripts/system/ScriptLoader.cpp
@@ -9,6 +9,7 @@ extern void AddSC_battleground();
 
 // custom
 extern void AddSC_npc_rentalmount();
+extern void AddSC_transmog();
 
 // examples
 extern void AddSC_example_creature();
@@ -340,6 +341,7 @@ void AddScripts()
 
     // custom
 	AddSC_npc_rentalmount();
+	AddSC_transmog();
 
     // examples
     AddSC_example_creature();
