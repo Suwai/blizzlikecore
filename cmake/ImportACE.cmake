71dc21c09692ce9aa853d1ccc469d1bf2a07d3b4
blizzlikegroup <correio13@gmail.com>
2013-10-16 19:05:54 -0300

    Updates.

--- cmake/ImportACE.cmake
+++ cmake/ImportACE.cmake
@@ -16,29 +16,14 @@
 # Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 #
 
-# Specify ace lib that was build externally
-# add_library(ace SHARED IMPORTED)
-# Sadly doesn't work in current version of cmake
-# add_dependencies(ace ACE_Project)
-# set_target_properties(ace PROPERTIES DEPENDS ACE_Project)
-
 if(WIN32)
   set(ACE_INCLUDE_DIR ${CMAKE_SOURCE_DIR}/dep/ACE_wrappers)
-  set(ACE_LIBRARIES_DIR ${CMAKE_SOURCE_DIR}/dep/ACE_wrappers/lib)
-  set(ACE_LIBRARIES optimized ACE debug ACEd)
+  set(ACE_LIBRARIES_DIR ${CMAKE_SOURCE_DIR}/Libraries)
+  set(ACE_LIBRARIES ACE)
 else()
   set(ACE_INCLUDE_DIR ${CMAKE_INSTALL_PREFIX}/include)
   set(ACE_LIBRARIES_DIR ${CMAKE_INSTALL_PREFIX}/lib)
   set(ACE_LIBRARIES ACE)
-endif()
-
-# Little Hack to remove the link warnings because of not found directories
-if(XCODE)
-  foreach(DIR ${ACE_LIBRARIES_DIR})
-    foreach(CONF ${CMAKE_CONFIGURATION_TYPES})
-      file(MAKE_DIRECTORY ${DIR}/${CONF})
-    endforeach(CONF)
-  endforeach(DIR)
 endif()
 
 link_directories(
@@ -46,14 +31,3 @@ link_directories(
 )
 
 set(HAVE_ACE_STACK_TRACE_H ON) # config.h.cmake
-
-if(WIN32)
-  foreach(DIR ${ACE_LIBRARIES_DIR})
-    install(
-      DIRECTORY ${DIR}/ DESTINATION ${LIBS_DIR}
-      FILES_MATCHING PATTERN "*.dll*" #"*.${LIB_SUFFIX}*"
-      PATTERN "pkgconfig" EXCLUDE
-    )
-  endforeach(DIR)
-endif()
-
