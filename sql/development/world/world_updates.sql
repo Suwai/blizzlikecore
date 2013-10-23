9a0453b90955922e0f597a99fc6d97486498f8f5
blizzlikegroup <correio13@gmail.com>
2013-10-23 12:59:46 -0200

    DB Update: Mission Gateways Murketh and Shaadraz (by: ysfl)

--- sql/development/world/world_updates.sql
+++ sql/development/world/world_updates.sql
@@ -13,3 +13,9 @@ UPDATE `gameobject_template` SET `ScriptName` = 'go_silithyst_geyser' WHERE `ent
 UPDATE `command` SET help = 'Syntax: .account create $account $password [$expansion]\r\n\r\nCreate account and set password to it. Optionally, you may also set another expansion for this account than the defined default value.' WHERE name = 'account create';
 -- NPC Netherspite Portal
 UPDATE `creature_template` SET `ScriptName` = 'npc_netherspite_portal' WHERE `entry` IN (17367,17368,17369);
+
+-- Update:Mission Gateways Murketh and Shaadraz (by ysfl <ysfllxcn@live.com>)
+REPLACE INTO `creature_ai_scripts` (`id`, `creature_id`, `event_type`, `event_inverse_phase_mask`, `event_chance`, `event_flags`, `event_param1`, `event_param2`, `event_param3`, `event_param4`, `action1_type`, `action1_param1`, `action1_param2`, `action1_param3`, `action2_type`, `action2_param1`, `action2_param2`, `action2_param3`, `action3_type`, `action3_param1`, `action3_param2`, `action3_param3`, `comment`) VALUES (1929201, 19292, 8, 0, 100, 0, 33655, -1, 0, 0, 33, 19292, 6, 0, 0, 0, 0, 0, 0, 0, 0, 0, 'Legion Transporter: Beta - Quest Credit on Dropping The Nether Modulator Spellhit (Quest: 10146)');
+REPLACE INTO `creature_ai_scripts` (`id`, `creature_id`, `event_type`, `event_inverse_phase_mask`, `event_chance`, `event_flags`, `event_param1`, `event_param2`, `event_param3`, `event_param4`, `action1_type`, `action1_param1`, `action1_param2`, `action1_param3`, `action2_type`, `action2_param1`, `action2_param2`, `action2_param3`, `action3_type`, `action3_param1`, `action3_param2`, `action3_param3`, `comment`) VALUES (1929101, 19291, 8, 0, 100, 0, 33655, -1, 0, 0, 33, 19291, 6, 0, 0, 0, 0, 0, 0, 0, 0, 0, 'Legion Transporter: Alpha - Quest Credit on Dropping The Nether Modulator Spellhit (Quest: 10129)');
+UPDATE `creature_template` SET `AIName`='EventAI' WHERE  `entry`=19291;
+UPDATE `creature_template` SET `AIName`='EventAI' WHERE  `entry`=19292;
